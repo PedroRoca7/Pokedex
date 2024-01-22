6c3666ca3ed502d88a97029a50a5528e0a796464
Pedro Henrique Roca Moreira <pedrohvalles7@gmail.com>
2024-01-22 11:22:55 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Pedro Henrique on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
